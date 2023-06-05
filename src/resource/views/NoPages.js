4107056ae89127416570eb06e8815e05a5f96d46
Triyonorifan11 <triyonorifan01@gmail.com>
2023-06-05 15:06:49 +0700

    delete navbar no page

--- src/resource/views/NoPages.js
+++ src/resource/views/NoPages.js
@@ -1,9 +1,6 @@
-import Navbar from "../../components/navbar";
-
 function NoPage(){
     return (
       <>
-      <Navbar/>
       <h1 className="flex justify-center align-middle mt-60">This NoPage</h1>
       </>
     )
